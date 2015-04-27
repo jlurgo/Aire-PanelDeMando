4cee3065a5613a0b7a5b74788527304bd45f6418
Javier Lurgo (ntbk) <jlurgo@gmail.com>
2015-04-27 04:44:18 -0300

    recepcion multiples emisores

--- NodoSesionSeriePG.js
+++ NodoSesionSeriePG.js
@@ -49,7 +49,9 @@ NodoSesionSeriePG.prototype.recibirMensaje = function(mensaje){
 };
 
 NodoSesionSeriePG.prototype.recibirIntSerie = function(int_recibido){  
-	if(int_recibido != 13) this.buffer_entrada_serie += String.fromCharCode.apply(int_recibido);
+	if(this.verbose) console.log("conector serie " + _this.idNodo + " recibió:",  int_recibido);
+	if(int_recibido != 13) 
+		this.buffer_entrada_serie += String.fromCharCode.apply(int_recibido);
 	else {
 		var mensaje;
 		try{
